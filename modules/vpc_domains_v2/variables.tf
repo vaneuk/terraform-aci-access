372e1bfabb9a0db03e639034512fcb750684115f
Ivan Sinyagovskiy <vaneuk@gmail.com>
2021-04-24 14:07:18 +0300

    formatting

--- modules/vpc_domains_v2/variables.tf
+++ modules/vpc_domains_v2/variables.tf
@@ -5,21 +5,21 @@ terraform {
 variable "vpc_domains" {
   description = "Create Explicit VPC Protection Group(s)"
   type = map(object({
-    annotation_domain        = optional(string)
-    domain_policy            = optional(string)
-    name                     = optional(string)
-    node1_id                 = optional(number)
-    node2_id                 = optional(number)
-    vpc_domain_id            = optional(number)
+    annotation_domain = optional(string)
+    domain_policy     = optional(string)
+    name              = optional(string)
+    node1_id          = optional(number)
+    node2_id          = optional(number)
+    vpc_domain_id     = optional(number)
   }))
   default = {
     default = {
-      annotation_domain        = ""
-      domain_policy            = "default"
-      name                     = "leaf101-102-vpc"
-      node1_id                 = 201
-      node2_id                 = 202
-      vpc_domain_id            = 201
+      annotation_domain = ""
+      domain_policy     = "default"
+      name              = "leaf101-102-vpc"
+      node1_id          = 201
+      node2_id          = 202
+      vpc_domain_id     = 201
     }
   }
 }
@@ -27,12 +27,12 @@ variable "vpc_domains" {
 locals {
   vpc_domains = {
     for k, v in var.vpc_domains : k => {
-      annotation_domain        = (v.annotation_domain != null ? v.annotation_domain : "")
-      domain_policy            = coalesce(v.domain_policy, "default")
-      name                     = coalesce(v.name, "leaf101-102-vpc")
-      node1_id                 = coalesce(v.node1_id, 201)
-      node2_id                 = coalesce(v.node2_id, 202)
-      vpc_domain_id            = coalesce(v.vpc_domain_id, 201)
+      annotation_domain = (v.annotation_domain != null ? v.annotation_domain : "")
+      domain_policy     = coalesce(v.domain_policy, "default")
+      name              = coalesce(v.name, "leaf101-102-vpc")
+      node1_id          = coalesce(v.node1_id, 201)
+      node2_id          = coalesce(v.node2_id, 202)
+      vpc_domain_id     = coalesce(v.vpc_domain_id, 201)
     }
   }
 }
